1d72f482c842bce4dec9dc2f1d8e532833da59b6
tehknesolutions <tehknesolutions@gmail.com>
2026-09-23 17:08:05 -0300

    feat: establish Playable V3 runtime visual default

---
+++ src/game/player/PlayableV3RuntimeDefault.ts
@@ -0,0 +1,6 @@
+export const PLAYABLE_V3_RUNTIME_DEFAULT = Object.freeze({
+  assetContractId: 'ROACH_PLAYABLE_V3',
+  premiumVisualByDefault: true,
+  legacyVisualRequiresExplicitOptIn: true,
+  sceneMaySilentlyDowngrade: false,
+} as const);
