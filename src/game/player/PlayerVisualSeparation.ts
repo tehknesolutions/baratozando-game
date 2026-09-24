df822f6e0128610b9dac4355bf67824105056459
tehknesolutions <tehknesolutions@gmail.com>
2026-09-24 14:19:57 -0300

    feat(V3-03): codify player visual authority split

---
+++ src/game/player/PlayerVisualSeparation.ts
@@ -0,0 +1,8 @@
+export const PLAYER_VISUAL_SEPARATION = Object.freeze({
+  actorOwnsPremiumTexture: false,
+  actorOwnsPremiumScale: false,
+  actorHasPhysicsBody: true,
+  visualOwnsPremiumTexture: true,
+  visualOwnsPremiumScale: true,
+  visualHasPhysicsBody: false,
+} as const);
